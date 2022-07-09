afafea8fe6ab38a2cb490f91334050b5dceffe74
axel <huertaaxel1003@gmail.com>
2022-07-08 23:39:58 -0300

    actualizado

---
+++ base/app.js
@@ -0,0 +1,50 @@
+//var  weather = prompt("how is the weather??");
+
+//if (weather === "rain"){
+//    console.log("grab your umbrella");
+//} else {
+//    console.log("wear you sunglasses");
+//}
+
+// Chapter 03-----
+
+//funtion
+
+
+//ES6
+function sayMyName(){
+    console.log("name..")
+    console.log("name..")
+    console.log("name..")
+    console.log("name..")
+}
+
+//arrow funtions
+
+let valor=prompt("ingrese aperation")
+
+const suma = (a, b)=>{
+    return a + b
+}
+const resta = (a, b)=>{
+    return a - b
+}
+const multi = (a, b)=>{
+    return a * b
+}
+const divi = (a, b)=>{
+    return a % b
+}
+
+
+
+switch (valor){
+    case "suma":
+        console.log(suma(10, 20))
+        break
+    case "resta":
+        console.log(resta())
+        break
+}
+    
+
